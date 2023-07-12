d379633a1c0dcc92f488c94a51a4b1a9b1052734
Gerke <piotrg2003@gmail.com>
2023-07-12 12:54:00 +0200

    deleted gtm and analytics from asystent ai

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -27,7 +27,6 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <main className={inter.className}>
       <Head>
-            <meta name="google-site-verification" content="FKFRw_OQ99WnUWH5bbf1OjTi7Cg2Xjh_6I6FnfzlPdA" />
             <meta name="viewport" content="initial-scale=1, width=device-width" />
 
             <meta property="og:title" content="Yepp AI | Your Custom AI" />
@@ -41,34 +40,6 @@ function MyApp({ Component, pageProps }: AppProps) {
             <meta name="twitter:image:alt" content="Yepp AI logo"></meta>
             <meta name="twitter:image" content="http://drive.google.com/uc?export=view&id=1oAavvRT1Es0jOOt67X62cwzM1Zh5k_de"></meta>
       </Head>
-      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-R8M9MNW23W`} />
-
-      <Script id="google analytics"strategy="lazyOnload">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-R8M9MNW23W');
-          `}
-      </Script>
-      <Script id="tag manager"strategy="lazyOnload">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-M27MWLQ');
-          `}
-      </Script>
-      <noscript
-        dangerouslySetInnerHTML={{
-          __html: `
-            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M27MWLQ"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe>
-          `,
-        }}
-      />
       <NotificationsProvider zIndex={2077}>
         <Component {...pageProps} />
       </NotificationsProvider>
