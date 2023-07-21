712da1705a79c1f92e785d438d1e5f08177f464a
Gerke <piotrg2003@gmail.com>
2023-07-21 12:37:10 +0200

    no console logs

--- components/Assets/DataPage.tsx
+++ components/Assets/DataPage.tsx
@@ -64,7 +64,6 @@ const DataPage = (props: {back: any, openedFolder: Folder | undefined, folders: 
                     Authorization: `${localStorage.getItem("token")}`
                 }
             });
-            console.log(folder.data);
             setDocuments(folder.data.documents);
             let onboardingSetp = localStorage.getItem("onboarding_step");
             if (onboardingSetp === "2" && folder.data.documents.length === 1) {
