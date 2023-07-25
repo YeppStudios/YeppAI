47130d760f2e695974d20edd2bf8cfc3fc0fe592
Gerke <piotrg2003@gmail.com>
2023-07-25 10:25:53 +0200

    GTM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -51,6 +51,34 @@ function MyApp({ Component, pageProps }: AppProps) {
             <meta name="twitter:image:alt" content="Yepp AI logo"></meta>
             <meta name="twitter:image" content="http://drive.google.com/uc?export=view&id=1FDGWNY8-NvmGRAApCeHz8mXV-h7uWBy6"></meta>
       </Head>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-WQM8PTR9`} />
+
+      <Script id="google analytics"strategy="lazyOnload">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-WQM8PTR9');
+          `}
+      </Script>
+      <Script id="tag manager"strategy="lazyOnload">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': 
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], 
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); 
+            })(window,document,'script','dataLayer','GTM-WQM8PTR9');
+          `}
+      </Script>
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WQM8PTR9" 
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>
+          `,
+        }}
+      />
       <NotificationsProvider zIndex={2077}>
         <Component {...pageProps} />
       </NotificationsProvider>
