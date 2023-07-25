bc34f9b80feb716529dd0923a1a40523d6d97841
Gerke <piotrg2003@gmail.com>
2023-07-25 19:31:05 +0200

    translations

--- components/Profile/BottomPanel.tsx
+++ components/Profile/BottomPanel.tsx
@@ -348,7 +348,7 @@ const BottomPanel = () => {
                                     scope="col"
                                     className="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
                                 >
-                                    Imię
+                                    Name
                                 </th>
                                 <th
                                     scope="col"
