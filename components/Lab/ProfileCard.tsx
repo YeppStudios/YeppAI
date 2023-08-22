824e4de803db21554ab07f9f81996d0bd6205b97
Remigiusz Mikolajewicz <remigiusz@thehumanaspect.com>
2023-08-22 15:11:34 +0200

    push everything for review

--- components/Lab/ProfileCard.tsx
+++ components/Lab/ProfileCard.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import { BsChevronRight } from "react-icons/bs";
 import Image, { StaticImageData } from "next/image";
+import styled from "styled-components";
 
 interface ProfileCardProps {
   icon: StaticImageData;
@@ -10,14 +11,22 @@ interface ProfileCardProps {
 
 export const ProfileCard: FC<ProfileCardProps> = ({ icon, name, type }) => {
   return (
-    <div className="flex gap-8">
-      <div className="rounded-full overflow-hidden">
-        <Image src={icon} width={50} height={50} alt="profile's icon" />
+    <div className="flex gap-[1vw] items-center">
+      <div className="rounded-full overflow-hidden h-[3vw] w-[3vw] relative">
+        <Image src={icon} fill alt="profile's icon" />
       </div>
       <div className="flex flex-col">
-        <h4>{name}</h4>
-        <span>{type}</span>
+        <NameText>{name}</NameText>
+        <TypeText className="text-slate-400 ">{type}</TypeText>
       </div>
     </div>
   );
 };
+
+const TypeText = styled.span`
+  font-size: min(0.9vw, 0.8rem);
+`;
+const NameText = styled.h4`
+  font-size: min(1.2vw, 1.2rem);
+  font-weight: 600;
+`;
--- pages/lab.tsx
+++ pages/lab.tsx
@@ -12,6 +12,9 @@ import { ProfileCard } from "@/components/Lab/ProfileCard";
 import { CampaignDropdown } from "@/components/Camapigns/Modal/CampaignDropdwon";
 import LinkedInIcon from "@/public/images/linkedin-color.png";
 import FacebookIcon from "@/public/images/facebook-color.png";
+import { IoRefreshOutline } from "react-icons/io5";
+import { FaGraduationCap } from "react-icons/fa";
+import { PlacementDropdown } from "@/components/Lab/PlacementDropdown";
 
 interface ProfileProps {
   id: number;
@@ -30,6 +33,22 @@ const LabPage = () => {
     },
     {
       name: "Facebook post",
+      icon: FacebookIcon,
+    },
+    {
+      name: "Linkedin 1",
+      icon: LinkedInIcon,
+    },
+    {
+      name: "Facebook 1",
+      icon: FacebookIcon,
+    },
+    {
+      name: "Linkedin 2",
+      icon: LinkedInIcon,
+    },
+    {
+      name: "Facebook 2",
       icon: FacebookIcon,
     },
   ];
@@ -53,16 +72,28 @@ const LabPage = () => {
       baseText:
         "222222 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam ad odio totam tenetur laborum accusamus ratione sunt velit libero quo eum maxime magni molestias quasi, recusandae deserunt veritatis in a cumque facere deleniti repellendus voluptatum, sed cum. Exercitationem, dolor enim.",
       description:
-        "22222 Here will be editable, detailed description of your tone of voice or persona",
+        "22222 Here will be editable, detailed description of your tone of voice or persona ",
     },
   ];
 
   const [currentProfile, setCurrentProfile] = useState<ProfileProps>();
-  const [searchPhrase, setSearchPhrase] = useState("");
+  const [searchPhrase, setSearchPhrase] = useState<string>("");
   const [filteredProfiles, setFilteredProfiles] =
     useState<ProfileProps[]>(profileData);
   const [openedCategory, setOpenedCategory] = useState<string>("");
   const [allChosenCategories, setAllChosenCategories] = useState<string[]>([]);
+  const [isSmallDevice, setIsSmallDevice] = useState<boolean>(false);
+
+  useEffect(() => {
+    const updateWindowSize = () => {
+      setIsSmallDevice(window.innerWidth < 1230);
+    };
+
+    window.addEventListener("resize", updateWindowSize);
+    return () => {
+      window.removeEventListener("resize", updateWindowSize);
+    };
+  }, []);
 
   useEffect(() => {
     const filtered = profileData.filter((profile) =>
@@ -86,119 +117,206 @@ const LabPage = () => {
   return (
     <Page className="flex flex-col gap-4">
       <NavigationBar />
-      <PageContainer className=" items-center h-full flex w-full text-black">
+      <PageContainer
+        className=" items-center h-full flex w-full text-black "
+        style={{ height: "100%" }}
+      >
         <div className="flex justify-between  w-full items-center">
           <div className="flex gap-4 items-center">
-            <h2 className="text-2xl">Tone of voice Lab</h2>
+            <LabTitle className="text-2xl">Tone of voice Lab</LabTitle>
             <div className="flex items-center justify-center py-2 px-6 rounded-2xl bg-slate-100">
-              <span className="text-center text-md uppercase">Beta</span>
+              <GradientText className="text-center text-md uppercase">
+                Beta
+              </GradientText>
             </div>
           </div>
-          <button>Create new</button>
+          <BlueButton>
+            <span className="flex items-center gap-[1vw]">
+              + <span> Create new </span>
+            </span>
+          </BlueButton>
         </div>
         <div></div>
       </PageContainer>
       <div className="flex gap-4">
         <PageContainer
-          className="w-[25%]  gap-8 flex flex-col text-black"
+          className="w-[20%]  gap-8 flex flex-col justify-between text-black "
           style={{ padding: "1.5rem" }}
         >
-          <InputContainer className="flex gap-2 items-center">
-            <AiOutlineSearch className="w-6 h-6 fill-black" />
-            <input
-              type="text"
-              placeholder="Search..."
-              className="bg-transparent w-full"
-              onChange={(e) => setSearchPhrase(e.target.value)}
-            />
-            <div className="flex items-center gap-4">
-              <BsCommand className="w-4 h-4 fill-black" />
-              <span>F</span>
-            </div>
-          </InputContainer>
-          <div className="w-full flex flex-col gap-4">
-            {filteredProfiles.map((profile) => {
-              const isProfileActicve = currentProfile?.id === profile.id;
-              return (
-                <div
-                  className={`flex border  w-full justify-between rounded-2xl p-2 ${
-                    isProfileActicve ? "bg-slate-300" : ""
-                  }`}
-                  onClick={() => setCurrentProfile(profile)}
-                  key={profile.id}
-                >
-                  <ProfileCard
-                    type={profile.type}
-                    name={profile.name}
-                    icon={profile.icon}
-                  />
-                  {isProfileActicve && (
-                    <div className="flex items-center">
-                      <BsChevronRight className="h-6 w-6" />
-                    </div>
-                  )}
+          <div className="flex flex-col gap-8 w-full">
+            <InputContainer className="flex gap-2 items-center">
+              <AiOutlineSearch className="w-6 h-6 fill-black" />
+              <input
+                type="text"
+                placeholder="Search..."
+                className="bg-transparent w-full"
+                onChange={(e) => setSearchPhrase(e.target.value)}
+              />
+              {!isSmallDevice && (
+                <div className="flex items-center gap-[1vw]">
+                  <BsCommand className="w-4 h-4 fill-black" />
+                  <span>F</span>
                 </div>
-              );
-            })}
+              )}
+            </InputContainer>
+            <div className="w-full flex flex-col gap-4">
+              {filteredProfiles.map((profile) => {
+                const isProfileActicve = currentProfile?.id === profile.id;
+                return (
+                  <div
+                    className={`flex border  w-full justify-between rounded-2xl p-2 ${
+                      isProfileActicve ? "bg-slate-300" : ""
+                    }`}
+                    onClick={() => setCurrentProfile(profile)}
+                    key={profile.id}
+                  >
+                    <ProfileCard
+                      type={profile.type}
+                      name={profile.name}
+                      icon={profile.icon}
+                    />
+                    {isProfileActicve && (
+                      <div className="flex items-center">
+                        <BsChevronRight className="h-[1.5vw] w-[1.5vw]" />
+                      </div>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div>
+            <BlueButton style={{ padding: "0.5rem 3vw 0.5rem 3vw" }}>
+              <span className="text-center items-center flex gap-2">
+                + {!isSmallDevice && <Text>Create New</Text>}
+              </span>
+            </BlueButton>
           </div>
         </PageContainer>
-        <PageContainer className="w-[40%] flex flex-col gap-8  text-black ">
-          <div className="flex gap-8 items-center w-full">
-            <BiBookAlt className="w-8 h-8" />
-            <h3>Base</h3>
+        <PageContainer
+          style={{ padding: "1.5rem 2vw 1.5rem 2vw" }}
+          className="w-[40%] flex flex-col gap-8 justify-between  text-black "
+        >
+          <div className="flex w-full flex-col gap-8 h-full">
+            <div className="flex gap-4 items-center w-full">
+              <BiBookAlt className="w-8 h-8" />
+              <h3 style={{ fontWeight: 800 }}>Base</h3>
+            </div>
+            <TextArea
+              padding="1rem"
+              height="100%"
+              value={currentProfile ? currentProfile.baseText : ""}
+              disabled={!currentProfile}
+              onChange={handleTextAreaChange}
+              className="overflow-scroll"
+            />
           </div>
-          <TextArea
-            padding="1rem"
-            height="100%"
-            value={currentProfile ? currentProfile.baseText : ""}
-            disabled={!currentProfile}
-            onChange={handleTextAreaChange}
-          />
+          <div>
+            <BlueButton className="flex gap-4 items-center">
+              <FaGraduationCap className="h-6 w-6" />
+              <span> Teach AI </span>
+            </BlueButton>
+          </div>
         </PageContainer>
-        <PageContainer className="w-[40%] text-black">
+        <PageContainer
+          style={{ padding: "1.5rem 2vw 1.5rem 2vw" }}
+          className="w-[40%] text-black overflow-hidden"
+        >
           {currentProfile && (
-            <div className="flex flex-col gap-8">
-              <div className="flex justify-between">
+            <div className="flex flex-col ">
+              <div className="flex justify-between items-center pb-[1vw]">
                 <ProfileCard
                   type={currentProfile.type}
                   icon={currentProfile.icon}
                   name={currentProfile.name}
                 />
-                <div className="flex gap-4">
-                  <button>Try now</button>
-                  <button>? About</button>
+                <div className="flex gap-4 h-full ">
+                  <button className="flex items-center  justify-center py-2 px-[1.5vw] rounded-2xl bg-slate-100">
+                    <GradientText>Try now</GradientText>
+                  </button>
+                  <button
+                    className="flex items-center  justify-center py-2 px-6 rounded-2xl bg-slate-100"
+                    style={{ fontWeight: 600 }}
+                  >
+                    ? About
+                  </button>
                 </div>
               </div>
-              <div className="flex flex-col gap-4">
-                <span>{`${currentProfile.name} type`}</span>
+              <div className="flex flex-col gap-[0.8vw]">
+                <span
+                  style={{ fontWeight: 500 }}
+                >{`${currentProfile.name} description`}</span>
                 <TextArea
                   padding="1rem"
-                  height="100%"
+                  height="10rem"
                   value={currentProfile.description}
+                  className="overflow-scroll"
                 />
-                <button className="self-end">Save</button>
+                <BlueButton
+                  className="self-end"
+                  style={{ paddingLeft: "3vw", paddingRight: "3vw" }}
+                >
+                  Save
+                </BlueButton>
               </div>
-              <div className="flex flex-col gap-4">
-                <h4 className="w-full border-b border-slate-200 pb-2">
+              <div className="flex flex-col gap-[0.8vw]">
+                <h4
+                  className="w-full border-b border-slate-200 pb-2"
+                  style={{ fontWeight: 600 }}
+                >
                   Example output
                 </h4>
-                <div className="flex justify-between items-center">
-                  <div className="flex flex-col gap-2 w-[45%]">
-                    <span>Placement</span>
-                    <CampaignDropdown
-                      category={socialMediaArray[0]}
-                      values={socialMediaArray}
-                      openedCategory={openedCategory}
-                      setOpenedCategory={setOpenedCategory}
-                      setAllChosenCategories={setAllChosenCategories}
-                    />
+                <div className={`flex  justify-between items-center `}>
+                  <div className="flex flex-col gap-2 w-full max-w-[15rem]">
+                    <span style={{ fontWeight: 500 }}>Placement</span>
+                    <PlacementDropdown placements={socialMediaArray} />
                   </div>
-                  <div className="flex flex-col gap-2 w-[50%]">
-                    <span>About</span>
-                    <InputContainer>
-                      <input type="text" />
-                    </InputContainer>
+                  <div className="flex flex-col gap-2 w-[18vw]">
+                    <span style={{ fontWeight: 500 }}>About</span>
+                    <div className="pl-2">
+                      <InputContainer>
+                        <input type="text" className="w-full" />
+                      </InputContainer>
+                    </div>
                   </div>
+                </div>
+              </div>
+              <div className="h-full flex items-center justify-center">
+                <div className="flex gap-4 h-56 flex-col p-6 shadow-2xl mt-[2vw] rounded-2xl border-2  border-[#ECEEF2]">
+                  <div
+                    className={`flex items-center w-full ${
+                      !isSmallDevice && "justify-between"
+                    }`}
+                  >
+                    <div className="flex gap-4  items-center w-full">
+                      <div className="relative w-8 h-8 rounded-xl overflow-hidden">
+                        <Image
+                          src={socialMediaArray[0].icon}
+                          fill
+                          alt="social media icon"
+                        />
+                      </div>
+                      <div className="flex flex-col gap-2">
+                        <GeneratedTextTitle>
+                          {socialMediaArray[0].name}
+                        </GeneratedTextTitle>
+                        <GeneratedTextDate>07.07.2023</GeneratedTextDate>
+                      </div>
+                    </div>
+                    <button className="flex px-[1.5vw] gap-4 items-center  py-1 bg-slate-200 rounded-2xl">
+                      <IoRefreshOutline className="w-4 h-4" />
+                      <Text style={{ fontWeight: 500 }}>Rewrite</Text>
+                    </button>
+                  </div>
+                  <GeneratedTextContainer>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Eum sequi animi ex exercitationem est deleniti quasi nemo
+                      accusantium natus doloremque consectetur, omnis excepturi
+                      harum nesciunt iusto laboriosam rerum esse nobis sint ea
+                      provident? Rem nobis ullam officia neque eius! Officia?
+                    </p>
+                  </GeneratedTextContainer>
                 </div>
               </div>
             </div>
@@ -208,6 +326,54 @@ const LabPage = () => {
     </Page>
   );
 };
+
+const LabTitle = styled.h2`
+  font-weight: 700;
+`;
+
+const Text = styled.p`
+  font-size: 0.9rem;
+`;
+
+const GeneratedTextContainer = styled.div`
+  display: flex;
+  overflow-y: scroll;
+`;
+
+const GradientText = styled.span`
+  font-weight: 600;
+  background: linear-gradient(40deg, #6578f8, #64b5ff);
+  -webkit-background-clip: text;
+  -moz-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  -moz-text-fill-color: transparent;
+`;
+
+const GeneratedTextTitle = styled.span`
+  font-size: 1rem;
+  font-weight: 600;
+`;
+
+const GeneratedTextDate = styled.span`
+  font-size: 0.8rem;
+  color: rgb(148 163 184);
+`;
+
+const ButtonText = styled.span`
+  font-size: 1vw;
+`;
+
+const BlueButton = styled.button`
+  display: flex;
+  gap: 1rem;
+  background: linear-gradient(40deg, #6578f8, #64b5ff);
+  /* padding: 0.5rem 5vw 0.5rem 5vw; */
+  padding: 0.5rem 3.5vw 0.5rem 3.5vw;
+  color: white;
+  justify-content: center;
+  align-items: center;
+  border-radius: 1rem;
+`;
 
 const InputContainer = styled.div`
   display: flex;
@@ -262,15 +428,9 @@ const PageContainer = styled.div`
   border: 2px solid #eaedf5;
   border-radius: 25px;
   padding: 1.5rem 3rem 1.5rem 3rem;
+  height: calc(100vh - 9rem);
+
   @media (max-width: 1023px) {
-    width: 100%;
-    display: block;
-    background-color: transparent;
-    align-items: flex-start;
-    min-height: 100vh;
-    padding: 0rem 0rem 4rem 0em;
-    box-shadow: none;
-    margin-bottom: 4rem;
   }
   border-radius: 20px;
   background-color: white;
