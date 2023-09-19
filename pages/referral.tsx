83aae165c0e8983a45da8f3dc86eaee9ad155bc6
Gerke <piotrg2003@gmail.com>
2023-09-19 22:29:46 +0200

    dashboard referral tab styling

--- pages/referral.tsx
+++ pages/referral.tsx
@@ -21,6 +21,9 @@ import Label from "@/components/Common/Label";
 import Input from "@/components/forms/Input";
 import ColorfulText from "@/components/Common/ColorfulText";
 import { MdContentCopy } from "react-icons/md";
+import referralBg from "../public/images/referralbg.png";
+import Centered from "@/components/Centered";
+import { BlueLoader } from "@/components/Common/Loaders";
 
 const Refferal = () => {
   interface HeaderDataType {
@@ -82,9 +85,9 @@ const Refferal = () => {
     datasets: [
       {
         data: chartData.map((item) => item.value),
-        backgroundColor: "rgb(226 232 240)",
-        borderRadius: 10,
-        hoverBackgroundColor: "blue",
+        backgroundColor: "#558FFF",
+        borderRadius: 12,
+        hoverBackgroundColor: "#558FFF",
       },
     ],
   };
@@ -140,6 +143,7 @@ const Refferal = () => {
 
   const [isSmallDevice, setIsSmallDevice] = useState(false);
   const [email, setEmail] = useState("");
+  const [transactions, setTransactions] = useState([]);
 
   useEffect(() => {
     const updateWindowSize = () => {
@@ -158,13 +162,13 @@ const Refferal = () => {
         <PageContainer
           style={{
             height: `${isSmallDevice ? "100%" : "auto"}`,
-            padding: "0.75rem 1rem 0.75rem 1rem;",
+            padding: "1.5rem 1.5rem 1.5rem 1.5rem",
           }}
         >
           <div className="flex lg:flex-row flex-col h-full">
             <div className="px-4 w-[50%] lg:border-r-2 border-slate-100">
               <SectionTitle>Affillate Dashboard</SectionTitle>
-              <span className="text-xl">Make money with Yepp</span>
+              <span className="text-xl">Share & earn money with Yepp</span>
             </div>
             <div className="w-[50%] lg:flex grid grid-cols-2 gap-4 items-center justify-around pt-8 lg:pt-0">
               {headerObject.map(({ icon, description, number }) => {
@@ -184,8 +188,8 @@ const Refferal = () => {
                 <ContainerTitle>Invite a friend</ContainerTitle>
                 <Image src={giftIcon} alt="gift icon" height={20} width={20} className="w-8 h-8" />
                 </div>
-              <p className="w-5/6 mt-2 text-sm">Gift your friend a $100 coupon code and claim $100 in cash once he/she stays.</p>
-              <div className="flex flex-wrap flex-col gap-8 h-full mt-6">
+              <p className="w-5/6 mt-2">Gift your friend a $100 coupon code and claim $100 in cash once he/she stays.</p>
+              <div className="flex flex-wrap flex-col gap-6 h-full mt-6">
                 <div className="pb-6 border-b-2 border-slate-100">
                 <div className="mb-2"><Label>Invite via email</Label></div>
                   <Input height="auto" padding="0.55rem" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="random@gail.com" />
@@ -199,15 +203,15 @@ const Refferal = () => {
           <PageContainer style={{ height: "24rem", width: "70%" }}>
             <div className="flex h-full flex-col">
               <div className="h-[30%] w-full  flex flex-col">
-                <div className="flex justify-between items-center">
+                <div className="flex justify-between">
                   <ContainerTitle>Revenue History</ContainerTitle>
-                  <button className="bg-slate-200 p-2 rounded-xl">
-                    withdraw now
+                  <button className="bg-[#F6F7FF] py-2 px-6 rounded-xl">
+                    Request withdrawal
                   </button>
                 </div>
-                <div className="flex ">
-                  <span>{`${totalSum}$`}</span>
-                  <span>total</span>
+                <div className="flex items-end">
+                  <span className="text-2xl font-black">{`$${totalSum}`}</span>
+                  <span className="text-slate-300 ml-2">total</span>
                 </div>
               </div>
               <div className="w-full h-[70%] flex items-center justify-center">
@@ -217,13 +221,72 @@ const Refferal = () => {
           </PageContainer>
         </div>
         <div className="flex flex-row gap-4 ">
-          <div className="flex h-[24rem] w-[30%] flex-col  gap-4">
-            <div className="h-[8rem] w-full bg-slate-300"></div>
-            <div className="h-[16rem] w-full bg-slate-300"></div>
+        <ReferralBackground background={referralBg}>
+          <div className="w-10/12">
+            <h2 className="text-3xl font-bold w-10/12">Refer Yepp and claim up to $200 total</h2>
+            <p className="w-5/6 mt-3">Gift your friend a $100 coupon code and claim $100 in cash once he/she stays.</p>
+            <button className="bg-white rounded-xl px-8 py-2 mt-8 text-black">Learn more</button>
           </div>
-          <PageContainer
-            style={{ height: "24rem", width: "70%" }}
-          ></PageContainer>
+
+        </ReferralBackground>
+          <PageContainer style={{ height: "24rem", width: "70%" }}>
+          <ContainerTitle>Recent Activity</ContainerTitle>
+                    <div className="">
+                        <div className="inline-block min-w-full py-2 align-middle">
+                            {transactions ?
+                            <table className="min-w-full border-separate border-spacing-0">
+                            <thead>
+                                <tr>
+                                <th
+                                    scope="col"
+                                    className="sticky top-0 z-10 boarder-b-2 border-[#F6F7FF] bg-white bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
+                                >
+                                    Email
+                                </th>
+                                <th
+                                    scope="col"
+                                    className="sticky top-0 z-10 hidden boarder-b-2 border-[#F6F7FF] bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:table-cell"
+                                >
+                                    Type
+                                </th>
+                                <th
+                                    scope="col"
+                                    className="sticky top-0 z-10 hidden boarder-b-2 border-[#F6F7FF] bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter lg:table-cell"
+                                >
+                                    Date
+                                </th>
+                                <th
+                                    scope="col"
+                                    className="sticky top-0 z-10 hidden boarder-b-2 border-[#F6F7FF] bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter lg:table-cell"
+                                >
+                                    Value
+                                </th>
+                                <th
+                                    scope="col"
+                                    className="sticky top-0 z-10 boarder-b-2 border-[#F6F7FF] bg-white bg-opacity-75 py-3.5 pl-3 pr-4 backdrop-blur backdrop-filter sm:pr-6 lg:pr-8"
+                                >
+                                    <span className="sr-only">Delete</span>
+                                </th>
+                                </tr>
+                            </thead>
+                            {transactions.length > 0 ?
+                            <></>
+                            :
+                            <div className="flex text-gray-400 ml-8 pt-8">
+                                <p>Here you will find your activity.</p>
+                            </div>   
+                            }
+                            </table>
+                            :
+                            <div className="mt-8">
+                                <Centered>
+                                    <BlueLoader />
+                                </Centered>    
+                            </div>           
+                            }
+                        </div>
+                    </div>
+          </PageContainer>
         </div>
       </div>
     </Page>
@@ -260,7 +323,7 @@ const PageContainer = styled.div`
   align-items: center;
   border: 2px solid #eaedf5;
   border-radius: 25px;
-  padding: 1.5rem;
+  padding: 1.5rem 1.75rem 1.5rem 1.75rem;
   height: calc(100vh - 9rem);
   font-weight: 500;
 
@@ -271,6 +334,19 @@ const PageContainer = styled.div`
   border-radius: 20px;
   background-color: white;
   box-shadow: 2px 2px 10px rgba(15, 27, 40, 0.15);
-`;
+`
+
+const ReferralBackground = styled.div<{ background: any }>`
+  width: 30%;
+  height: 24rem;
+  border-radius: 25px;
+  background-image: url(${({ background }) => background.src});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
 export default Refferal;
