56c0c72ba15b7b2c1a34cefcf2e62c476332b0d9
Gerke <piotrg2003@gmail.com>
2023-09-19 23:04:24 +0200

    referral for desktop layout v1

--- pages/referral.tsx
+++ pages/referral.tsx
@@ -192,11 +192,11 @@ const Refferal = () => {
               <div className="flex flex-wrap flex-col gap-6 h-full mt-6">
                 <div className="pb-6 border-b-2 border-slate-100">
                 <div className="mb-2"><Label>Invite via email</Label></div>
-                  <Input height="auto" padding="0.55rem" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="random@gail.com" />
+                  <div className="flex items-center justify-between gap-2"><Input height="auto" padding="0.6rem" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="random@gail.com" /><BlueBtn>Invite</BlueBtn></div>
                 </div>
                 <div>
                   <div className="mb-2"><Label>Invite via link</Label></div>
-                  <div className="px-4 py-[0.55rem] bg-[#F6F7FF] rounded-xl flex justify-between items-center"><div className="w-5/6 overflow-scroll"><ColorfulText>https://www.yepp.ai/dsgfasgsderg</ColorfulText></div><MdContentCopy /></div>
+                  <div className="px-4 py-[0.6rem] bg-[#F6F7FF] rounded-xl flex justify-between items-center"><div className="w-5/6 overflow-scroll"><ColorfulText>https://www.yepp.ai/dsgfasgsderg</ColorfulText></div><MdContentCopy className="cursor-pointer hover:scale95 transition"/></div>
                 </div>
               </div>
           </PageContainer>
@@ -205,7 +205,7 @@ const Refferal = () => {
               <div className="h-[30%] w-full  flex flex-col">
                 <div className="flex justify-between">
                   <ContainerTitle>Revenue History</ContainerTitle>
-                  <button className="bg-[#F6F7FF] py-2 px-6 rounded-xl">
+                  <button className="bg-[#F6F7FF] py-2 px-6 rounded-xl  hover:scale-95 transition hover:bg-[#EDEFFB]">
                     Request withdrawal
                   </button>
                 </div>
@@ -225,13 +225,13 @@ const Refferal = () => {
           <div className="w-10/12">
             <h2 className="text-3xl font-bold w-10/12">Refer Yepp and claim up to $200 total</h2>
             <p className="w-5/6 mt-3">Gift your friend a $100 coupon code and claim $100 in cash once he/she stays.</p>
-            <button className="bg-white rounded-xl px-8 py-2 mt-8 text-black">Learn more</button>
+            <button className="bg-white rounded-xl px-8 py-2 mt-8 text-black hover:scale-95 transition">Learn more</button>
           </div>
 
         </ReferralBackground>
           <PageContainer style={{ height: "24rem", width: "70%" }}>
           <ContainerTitle>Recent Activity</ContainerTitle>
-                    <div className="">
+                    <div className="mt-2">
                         <div className="inline-block min-w-full py-2 align-middle">
                             {transactions ?
                             <table className="min-w-full border-separate border-spacing-0">
@@ -239,31 +239,31 @@ const Refferal = () => {
                                 <tr>
                                 <th
                                     scope="col"
-                                    className="sticky top-0 z-10 boarder-b-2 border-[#F6F7FF] bg-white bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
+                                    className="sticky top-0 z-10 border-b-2 border-[#F6F7FF] bg-white bg-opacity-75 py-3.5 pl-2 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
                                 >
                                     Email
                                 </th>
                                 <th
                                     scope="col"
-                                    className="sticky top-0 z-10 hidden boarder-b-2 border-[#F6F7FF] bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:table-cell"
+                                    className="sticky top-0 z-10 hidden border-b-2 border-[#F6F7FF] bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:table-cell"
                                 >
                                     Type
                                 </th>
                                 <th
                                     scope="col"
-                                    className="sticky top-0 z-10 hidden boarder-b-2 border-[#F6F7FF] bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter lg:table-cell"
+                                    className="sticky top-0 z-10 hidden border-b-2 border-[#F6F7FF] bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter lg:table-cell"
                                 >
                                     Date
                                 </th>
                                 <th
                                     scope="col"
-                                    className="sticky top-0 z-10 hidden boarder-b-2 border-[#F6F7FF] bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter lg:table-cell"
+                                    className="sticky top-0 z-10 hidden border-b-2 border-[#F6F7FF] bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter lg:table-cell"
                                 >
                                     Value
                                 </th>
                                 <th
                                     scope="col"
-                                    className="sticky top-0 z-10 boarder-b-2 border-[#F6F7FF] bg-white bg-opacity-75 py-3.5 pl-3 pr-4 backdrop-blur backdrop-filter sm:pr-6 lg:pr-8"
+                                    className="sticky top-0 z-10 border-b-2 border-[#F6F7FF] bg-white bg-opacity-75 py-3.5 pl-3 pr-4 backdrop-blur backdrop-filter sm:pr-6 lg:pr-8"
                                 >
                                     <span className="sr-only">Delete</span>
                                 </th>
@@ -334,6 +334,56 @@ const PageContainer = styled.div`
   border-radius: 20px;
   background-color: white;
   box-shadow: 2px 2px 10px rgba(15, 27, 40, 0.15);
+`
+
+const SpecialPageContainer = styled.div`
+  color: black;
+  align-items: center;
+  border: 2px solid #eaedf5;
+  border-radius: 25px;
+  padding: 1.5rem 1.75rem 1.5rem 1.75rem;
+  height: calc(100vh - 9rem);
+  font-weight: 500;
+
+  @media (max-width: 1023px) {
+    height: 100vh;
+    padding: 1rem;
+  }
+  border-radius: 20px;
+  background-color: white;
+  box-shadow: 2px 2px 10px rgba(15, 27, 40, 0.15);
+`
+
+const BlueBtn = styled.div`
+    height: 2.8rem;
+    padding: 0 2rem 0 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 15px;
+    box-shadow: inset 2px 2px 6px rgba(22, 27, 29, 0.23), inset -2px -2px 4px #FAFBFF, 1px 1px 3px rgba(22, 27, 29, 0.23);
+    border: solid 3px transparent;
+    background-origin: border-box;
+    background-clip: padding-box, border-box;
+    position: relative;
+    white-space: nowrap;
+    color: white;
+    font-weight: 500;
+    background: linear-gradient(40deg, #6578F8, #64B5FF);
+    background-size: 110%;
+    background-position-x: -0.5rem;
+    align-items: center;
+    transition: all 0.4s ease;
+    cursor: pointer;
+    &:hover {
+      box-shadow: none;
+      transform: scale(0.95);
+    }
+    @media (max-width: 1023px) {
+      margin-left: 0;
+      margin-right: 0rem;
+      margin-top: 1rem;
+    }
 `
 
 const ReferralBackground = styled.div<{ background: any }>`
